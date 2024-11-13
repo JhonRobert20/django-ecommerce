6eb5de81db4a8d57d65a592922ffe306b582028b
JhonRobert20 <jhonbara51214@gmail.com>
2024-11-13 19:53:03 +0100

    feat: new models

---
+++ root/client/models.py
@@ -0,0 +1,25 @@
+import uuid
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Client(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+    phone_extension = models.CharField(max_length=5, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+
+class Directions(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    direction = models.CharField(max_length=500)
+    name = models.CharField(max_length=500)
+    is_principal = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Direction for {self.client.user.first_name} {self.client.user.last_name}"
--- root/products/models.py
+++ root/products/models.py
@@ -1,1 +1,31 @@
-# Create your models here.
+import uuid
+
+from django.db import models
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductDetails(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    stock = models.IntegerField()
+    grape_types = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Details for {self.product.name}"
