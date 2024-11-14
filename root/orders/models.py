d8eaa8be0cb71894d42b9d9927f1bd751a592f7e
JhonRobert20 <jhonbara51214@gmail.com>
2024-11-14 12:52:22 +0100

    fix: correct models

--- root/orders/models.py
+++ root/orders/models.py
@@ -23,22 +23,22 @@ class OrderState(Model):
 
 class Order(Model):
     id = UUIDField(primary_key=True, default=uuid4, editable=False)
-    client = ForeignKey(Client, on_delete=SET_NULL)
-    total = FloatField(null=False, blank=False)
+    client = ForeignKey(Client, on_delete=SET_NULL, null=True)
+    total = FloatField(null=False)
     date = DateTimeField()
-    order_state = ForeignKey(OrderState, on_delete=SET_NULL)
+    order_state = ForeignKey(OrderState, on_delete=SET_NULL, null=True)
 
 
 class OrderDetail(Model):
     order = OneToOneField(Order, on_delete=CASCADE)
-    product = ForeignKey(Product, on_delete=SET_NULL, blank=False)
+    product = ForeignKey(Product, on_delete=SET_NULL, null=True)
     unit_price = FloatField(null=False)
     amount = IntegerField(null=False)
 
 
 class Payment(Model):
     id = UUIDField(primary_key=True, default=uuid4, editable=False)
-    client = ForeignKey(Client, on_delete=SET_NULL)
-    order = ForeignKey(Order, on_delete=SET_NULL)
-    payment_method = CharField(help_text="WIP")
+    client = ForeignKey(Client, on_delete=SET_NULL, null=True)
+    order = ForeignKey(Order, on_delete=SET_NULL, null=True)
+    payment_method = CharField(help_text="WIP", max_length=255)
     date = DateTimeField()
