67be46d3b6e0ab014178f6a9dce8ab87a5479ed7
JhonRobert20 <jhonbara51214@gmail.com>
2024-11-14 13:11:00 +0100

    refactor: alter client model

--- root/clients/models.py
+++ root/clients/models.py
@@ -9,10 +9,8 @@ class Client(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone = models.IntegerField(max_length=20)
-    phone_extension = models.IntegerField(
-        max_length=5, blank=True, null=True, help_text="Exclude +"
-    )
+    phone = models.IntegerField()
+    phone_extension = models.IntegerField(blank=True, null=True, help_text="Exclude +")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
