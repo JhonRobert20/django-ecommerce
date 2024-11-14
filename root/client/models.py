cc62c794448b901c344ce7a18cb93a150e960eb2
JhonRobert20 <jhonbara51214@gmail.com>
2024-11-14 12:14:10 +0100

    refactor: client have all the logic

--- root/client/models.py
+++ root/client/models.py
@@ -1,18 +1,19 @@
 import uuid
 
-from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.db import models
 
 
 class Client(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(required=True, unique=True)
     phone = models.CharField(max_length=20)
     phone_extension = models.CharField(max_length=5, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.user.first_name} {self.user.last_name}"
+        return f"{self.first_name} {self.last_name}"
 
 
 class Directions(models.Model):
@@ -23,7 +24,10 @@ class Directions(models.Model):
     is_principal = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Direction for {self.client.user.first_name} {self.client.user.last_name}"
+        return (
+            f"Direction for {self.client.first_name} {self.client.last_name} "
+            f"with name {self.name}: {self.direction}"
+        )
 
     def clean(self):
         if (
