e08e5d72d779e5ea2981401fb85241a638ab946c
JhonRobert20 <jhonbara51214@gmail.com>
2024-11-14 13:17:23 +0100

    feat: orders admin

--- root/orders/admin.py
+++ root/orders/admin.py
@@ -1,1 +1,23 @@
-# Register your models here.
+from django.contrib.admin import ModelAdmin, register
+
+from orders.models import Order, OrderDetail, OrderState, Payment
+
+
+@register(OrderState)
+class OrderStateAdmin(ModelAdmin):
+    pass
+
+
+@register(Order)
+class OrderAdmin(ModelAdmin):
+    pass
+
+
+@register(OrderDetail)
+class OrderDetailAdmin(ModelAdmin):
+    pass
+
+
+@register(Payment)
+class PaymentAdmin(ModelAdmin):
+    pass
