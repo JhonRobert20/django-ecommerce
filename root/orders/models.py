6d53bb511dedaf5165ea5703beb871df92072244
JhonRobert20 <jhonbara51214@gmail.com>
2024-11-14 12:37:40 +0100

    feat: models for ecommerce

--- root/orders/models.py
+++ root/orders/models.py
@@ -1,1 +1,44 @@
-# Create your models here.
+from uuid import uuid4
+
+from client.models import Client
+from django.db.models import (
+    CASCADE,
+    SET_NULL,
+    CharField,
+    DateTimeField,
+    FloatField,
+    ForeignKey,
+    IntegerField,
+    Model,
+    OneToOneField,
+    UUIDField,
+)
+from products.models import Product
+
+
+class OrderState(Model):
+    id = UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = CharField(max_length=255)
+
+
+class Order(Model):
+    id = UUIDField(primary_key=True, default=uuid4, editable=False)
+    client = ForeignKey(Client, on_delete=SET_NULL)
+    total = FloatField(required=True, blank=False)
+    date = DateTimeField()
+    order_state = ForeignKey(OrderState)
+
+
+class OrderDetail(Model):
+    order = OneToOneField(Order, on_delete=CASCADE)
+    product = ForeignKey(Product, on_delete=SET_NULL, blank=False)
+    unit_price = FloatField(required=True)
+    amount = IntegerField(required=True)
+
+
+class Payment(Model):
+    id = UUIDField(primary_key=True, default=uuid4, editable=False)
+    client = ForeignKey(Client, on_delete=SET_NULL)
+    order = ForeignKey(Order, on_delete=SET_NULL)
+    payment_method = CharField(help_text="WIP")
+    date = DateTimeField()
