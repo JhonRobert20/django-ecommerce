c1904330c076625bcc80d89a557d6729a0b5950e
JhonRobert20 <jhonbara51214@gmail.com>
2024-11-14 13:19:05 +0100

    feat: products admin

--- root/products/admin.py
+++ root/products/admin.py
@@ -1,1 +1,18 @@
-# Register your models here.
+from django.contrib.admin import ModelAdmin, register
+
+from products.models import Category, Product, ProductDetails
+
+
+@register(Product)
+class ProductAdmin(ModelAdmin):
+    pass
+
+
+@register(ProductDetails)
+class ProductDetailsAdmin(ModelAdmin):
+    pass
+
+
+@register(Category)
+class CategoryAdmin(ModelAdmin):
+    pass
